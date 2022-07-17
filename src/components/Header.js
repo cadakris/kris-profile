5ad2b43955eb572f54c069b3456db7a55ac3337c
Kristen <kriscadacio@gmail.com>
2022-07-16 21:16:28 -0400

    Included additional animation on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,41 +11,48 @@ function Header() {
           <header id="noColorHeader">
           <div className="headerLeftStyle">
           <h1>
-            <span className="titleFirstName">kristen</span>
+            <div className="overflowHidden">
+              <span className="titleFirstName dropIn1b">kristen</span>
+            </div>
+
+            <div className="overflowHidden">
             {/* LINK CONTAINERS */}
+              <div className="dropIn4">
               <div className="webLinksContainer">
-              <a href="#about">About</a>
-              <a href="#projects">Projects</a>
-              <a className="verticalLine" href="#contact">Contact</a>
-            </div> 
+                <a href="#about">About</a>
+                <a href="#projects">Projects</a>
+                <a className="verticalLine" href="#contact">Contact</a>
+              </div> 
 
-            <div className="iconContainer">
-              <a
-                className="headerIcons"
-                href="https://github.com/cadakris"
-                target='_blank'
-                rel="noopener"
-                aria-label='Github'
-              > <AiFillGithub size={30}/>
-            </a>
+              <div className="iconContainer">
+                <a
+                  className="headerIcons"
+                  href="https://github.com/cadakris"
+                  target='_blank'
+                  rel="noopener"
+                  aria-label='Github'
+                > <AiFillGithub size={30}/>
+                </a>
 
-            <a
-                className="headerIcons"
-                href="https://www.linkedin.com/in/kristen-cadacio-408b3321/"
-                target='_blank'
-                rel="noopener"
-                aria-label='LinkedIn'
-              ><BsLinkedin size={30}/>
-            </a>
+                <a
+                    className="headerIcons"
+                    href="https://www.linkedin.com/in/kristen-cadacio-408b3321/"
+                    target='_blank'
+                    rel="noopener"
+                    aria-label='LinkedIn'
+                  ><BsLinkedin size={30}/>
+                </a>
 
-            <a
-                className="headerIcons"
-                href="https://medium.com/@kriscadacio"
-                target='_blank'
-                rel="noopener"
-                aria-label='Medium'
-              ><AiOutlineMedium size={30} />
-            </a>
+                <a
+                  className="headerIcons"
+                  href="https://medium.com/@kriscadacio"
+                  target='_blank'
+                  rel="noopener"
+                  aria-label='Medium'
+                ><AiOutlineMedium size={30} />
+                </a>
+              </div>
+              </div>
             </div>
           </h1>
           </div>
@@ -54,10 +61,14 @@ function Header() {
           <div className="content rightBackground">
             <div className="headerAdjectiveContainer">
               <h1>
-                <span className="titleLastName">cadacio</span><br/>
-                  <div className="wrapper">
+                <div className="overflowHidden dropIn1">
+                  <span className="titleLastName ">cadacio</span><br/>
+                </div>
+
+                <div className="overFlowHidden">
+                  <div className="wrapper dropIn2">
                     <span className="titleJob">software developer and </span>
-                      <div className="adjectiveWords">
+                    <div className="adjectiveWords dropIn3">
                         <span className="flipAdjective">plant mom</span>
                         <span className="flipAdjective">avid hiker</span>
                         <span className="flipAdjective">coffee shop addict</span>
@@ -65,6 +76,7 @@ function Header() {
                         <span className="flipAdjective">ice cream lover</span>
                       </div>
                   </div>
+                </div>
               </h1>
             </div>
           </div>
